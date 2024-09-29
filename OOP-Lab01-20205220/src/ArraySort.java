793c855ea08090689b46c23aa4f09092e787d714
SonPham <phamcongson297@gmail.com>
2024-09-29 17:52:02 +0700

    add ArraySort.java

---
+++ OOP-Lab01-20205220/src/ArraySort.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArraySort {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int[] array;
+		System.out.print("Enter the size of the array: ");
+		int size = scanner.nextInt();
+		array = new int[size];
+		for (int i = 0; i < size; i++) {
+			System.out.print("Enter element " + (i + 1) + ": ");
+			array[i] = scanner.nextInt();
+		}
+		Arrays.sort(array);
+
+		int sum = 0;
+		for (int num : array) {
+			sum += num;
+		}
+
+		double average = (double) sum / size;
+
+		System.out.println("Sorted array: " + Arrays.toString(array));
+		System.out.println("Sum of array : " + sum);
+		System.out.println("Average of array : " + average);
+		scanner.close();
+	}
+
+}
