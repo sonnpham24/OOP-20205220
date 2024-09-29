342ce5fe6316a9d44a4cfa144504e00460e119d9
SonPham <phamcongson297@gmail.com>
2024-09-29 17:20:16 +0700

    fix ChoosingOption.java theo question

--- OOP-Lab01-20205220/src/ChoosingOption.java
+++ OOP-Lab01-20205220/src/ChoosingOption.java
@@ -4,9 +4,11 @@ import javax.swing.JOptionPane;
 public class ChoosingOption {
 
 	public static void main(String[] args) {
-		int option = JOptionPane.showConfirmDialog(null, "Do you want to change to the first class ticket?");
+		int option = JOptionPane.showConfirmDialog(null, "Do you want to change to the first class ticket?",
+				"Choose an option", JOptionPane.YES_NO_OPTION);
 
 		JOptionPane.showMessageDialog(null, "You've chosen: " + (option == JOptionPane.YES_OPTION ? "Yes" : "No"));
+
 		System.exit(0);
 	}
 
